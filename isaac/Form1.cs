c855a361a4deba3c86fd127900afb46d3aa9d850
IvanMi773 <imihajlovskij23@gmail.com>
2020-05-29 21:45:04 +0300

    До проекту додано текстури, створенна  границя на головному екрані

--- isaac/Form1.cs
+++ isaac/Form1.cs
@@ -12,8 +12,8 @@ namespace isaac
 {
     public partial class Form1 : Form
     {
-        private int width = 700;
-        private int height = 700;
+        private int width = 706;
+        private int height = 729;
 
         private int sizeOfSides = 30;
 
@@ -29,24 +29,62 @@ namespace isaac
 
         private void GenerateMap()
         {
-            for (int i = 0; i <= width / sizeOfSides; i++)
+            for (int i = 0; i < width / sizeOfSides; i++)
             {
                 PictureBox pic = new PictureBox();
 
                 pic.BackColor = Color.Black;
                 pic.Location = new Point(0, sizeOfSides * i);
-                pic.Size = new Size(width, 1);
+                pic.Size = new Size(width - 10, 1);
                 this.Controls.Add(pic);
+
+                PictureBox borderLeft = new PictureBox();
+
+                string path = System.IO.Path.GetFullPath(@"textures\stonebricksmooth_cracked.png");
+                borderLeft.Image = Image.FromFile(path);
+                borderLeft.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                borderLeft.Location = new Point(0, sizeOfSides * i);
+                borderLeft.Size = new Size(sizeOfSides, sizeOfSides);
+                this.Controls.Add(borderLeft);
+
+                PictureBox borderRight = new PictureBox();
+
+                borderRight.Image = Image.FromFile(path);
+                borderRight.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                borderRight.Location = new Point(661, sizeOfSides * i);
+                borderRight.Size = new Size(sizeOfSides, sizeOfSides);
+                this.Controls.Add(borderRight);
             }
 
-            for (int i = 0; i <= height / sizeOfSides; i++)
+            for (int i = 0; i < width / sizeOfSides; i++)
             {
                 PictureBox pic = new PictureBox();
 
                 pic.BackColor = Color.Black;
                 pic.Location = new Point(sizeOfSides * i, 0);
-                pic.Size = new Size(1, height);
+                pic.Size = new Size(1, height - 20);
                 this.Controls.Add(pic);
+
+                PictureBox borderUp = new PictureBox();
+
+                string path = System.IO.Path.GetFullPath(@"textures\stonebricksmooth_cracked.png");
+                borderUp.Image = Image.FromFile(path);
+                borderUp.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                borderUp.Location = new Point(sizeOfSides * i, 0);
+                borderUp.Size = new Size(sizeOfSides, sizeOfSides);
+                this.Controls.Add(borderUp);
+
+                PictureBox borderDown = new PictureBox();
+
+                borderDown.Image = Image.FromFile(path);
+                borderDown.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                borderDown.Location = new Point(sizeOfSides * i, 661);
+                borderDown.Size = new Size(sizeOfSides, sizeOfSides);
+                this.Controls.Add(borderDown);
             }
         }
     }
